6b9a4b18c457cfc3a8e1fcb1c954cfb1e24f603f
Joe <joe>
2015-08-27 00:01:57 +1000

    new game WIP

--- client/newGame.jsx
+++ client/newGame.jsx
@@ -1,13 +1,37 @@
 FlowRouter.route('/new', {
     action() {
-        ReactLayout.render(MainLayout, <NewGame />);
+        ReactLayout.render(MainLayout, {content: <NewGame />});
     }
 });
 
 var NewGame = React.createClass({
     render() {
         return (
-            <h1>New Game</h1>
+            <div className="container">
+                <h1 style={{backgroundColor:'blue'}}>Character Creation</h1>
+
+                <div className="form-horizontal">
+                    <div className="form-group">
+                        <label htmlFor="name1" className="control-label col-sm-2"> Character name: </label>
+
+                        <div className="col-sm-10">
+                            <input id="name1" className="form-control" placeholder="Name..."/>
+                        </div>
+                    </div>
+                </div>
+
+                // attributes
+
+
+                // icon
+
+                // difficulty
+
+                <div>
+                    <div className="btn btn-success">Start Game</div>
+                    <div className="btn btn-default pull-right">Back</div>
+                </div>
+            </div>
         )
     }
 });
