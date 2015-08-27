d09780e31ee501c56df8b1aa2557bb3cffba3862
Joseph Ni <joseph.ni@gmail.com>
2015-08-27 20:30:39 +1000

    abc

--- client/main.js
+++
@@ -1,6 +0,0 @@
-Router.route('/', 'main');
-
-// fade in sub after page has rendered
-Template.main.onRendered(function () {
-    Meteor.setTimeout(function () {$('#subtitle').addClass('fadein');}, 1000);
-});
--- client/newGame.js
+++
@@ -1,2 +0,0 @@
-Router.route('new', 'newGame');
-
